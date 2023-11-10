ec4d3d936c1edf0d3bd447bce260b6d24259905e
monkeycoder-09 <monkeycoder-09@users.noreply.github.com>
2023-11-10 20:10:00 +0530

    Hover effect

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let i=0; i < 16 * 16; i++) {
         const gridSquare = document.createElement("div");
         gridSquare.classList.add("grid-square");
+
+        gridSquare.addEventListener("mouseenter", function() {
+            gridSquare.classList.add("hovered");
+        });
+
+        gridSquare.addEventListener("mouseleave", function(){
+            gridSquare.classList.remove("hovered");
+        });
+
         container.appendChild(gridSquare);
     }
 });
