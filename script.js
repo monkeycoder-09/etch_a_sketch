eb0e446f7b04e6a872cc2df5bf675f73294bf11a
monkeycoder-09 <monkeycoder-09@users.noreply.github.com>
2023-11-11 01:12:33 +0530

    Basic Etch-A-Sketch structure

--- script.js
+++ script.js
@@ -1,27 +1,53 @@
-let isDrawing = false;
-
-
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const container = document.querySelector(".container");
+    const resetButton = document.querySelector(".reset-button");
+    let isDrawing = false;
 
-    for (let i=0; i < 16 * 16; i++) {
-        const gridSquare = document.createElement("div");
-        gridSquare.classList.add("grid-square");
+    function createGrid(size) {
+        container.innerHTML = "";
 
-        gridSquare.addEventListener("mousedown", function() {
-            isDrawing = true;
-        });
+        const containerWidth = 500;
+        const squareWidth = containerWidth / size - 2;
 
-        gridSquare.addEventListener("mouseup", function() {
-            isDrawing = false;
-        });
+        for (let i = 0; i < size * size; i++) {
+            const gridSquare = document.createElement("div");
+            gridSquare.classList.add("grid-square");
+            gridSquare.style.width = `${squareWidth}px`;
+            gridSquare.style.height = `${squareWidth}px`;
+            // gridSquare.style.width = `calc(${squareWidth}% - 2px)`;
+            // gridSquare.style.paddingBottom = `calc(${squareWidth}% - 2px)`;
 
-        gridSquare.addEventListener("mousemove", function(){
-            if (isDrawing) {
-                gridSquare.classList.add("drawn")
-            }
-        });
+            gridSquare.addEventListener("mousedown", function () {
+                isDrawing = true;
+            });
 
-        container.appendChild(gridSquare);
+            gridSquare.addEventListener("mouseup", function () {
+                isDrawing = false;
+            });
+
+            gridSquare.addEventListener("mousemove", function () {
+                if (isDrawing) {
+                    gridSquare.classList.add("drawn");
+                }
+            });
+
+            container.appendChild(gridSquare);
+        }
     }
+
+    resetButton.addEventListener("click", function () {
+        const newSize = prompt("Enter the number of squares per side (max: 16): ");
+        const validatedSize = parseInt(newSize, 10);
+
+        // Check if the input is a number and within the limit
+        if (!isNaN(validatedSize) && validatedSize > 0 && validatedSize <= 16) {
+            createGrid(validatedSize);
+        } else {
+            alert("Please enter a valid number between 1 and 16.");
+        }
+    });
+
+
+    // Initial grid creation
+    createGrid(16); // You can set the initial size here
 });
